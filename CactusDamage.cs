f386eb5fdcbf4da7305e1a1822c7ae1b62082465
charlie brannan <charliebrannan154@gmail.com>
2019-12-29 22:38:31 +1100

    upload #1

---
+++ CactusDamage.cs
@@ -0,0 +1,46 @@
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace CactusDamage
+{
+	public class CactusDamage : Mod
+	{
+		public CactusDamage()
+		{
+		}
+	}
+
+	public class CDPlayer : ModPlayer
+	{
+		public override void PostUpdate()
+		{
+			if (player.IsPlayerTouchingTile(TileID.Cactus))
+			{
+				player.Hurt(PlayerDeathReason.ByCustomReason(" got spiked to death by a cactus. Ouch!"), 9, player.direction);
+			}
+		}
+	}
+
+	internal static class Extensions
+	{
+		public static bool IsPlayerTouchingTile(this Player P, int TileID)
+		{
+			Point16 curTilePos = P.position.ToTileCoordinates16();
+			for (int curX = curTilePos.X; curX < curTilePos.X + 3; curX++)
+			{
+				for (int curY = curTilePos.Y; curY < curTilePos.Y + 4; curY++)
+				{
+					Rectangle tileRect = new Rectangle(curX * 16, curY * 16, 16, 16);
+					if (P.getRect().Intersects(tileRect) && Main.tile[curX, curY].type == TileID)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
