d1f6d485f8448eea687e894b0188def0c23edd61
charlie brannan <charliebrannan154@gmail.com>
2020-01-19 13:51:00 +1100

    migrated to library

--- CactusDamage.cs
+++ CactusDamage.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Microsoft.Xna.Framework;
+using GoodProLib.GData;
 
 namespace CactusDamage
 {
@@ -14,31 +14,8 @@ namespace CactusDamage
 	{
 		public override void PostUpdate()
 		{
-			if (player.IsPlayerTouchingTile(TileID.Cactus))
-			{
-				player.Hurt(PlayerDeathReason.ByCustomReason(" got spiked to death by a cactus. Ouch!"), Main.rand.Next(6, 8), player.direction);
-			}
-		}
-	}
-
-	internal static class Extensions
-	{
-		//Thanks Aqua
-		public static bool IsPlayerTouchingTile(this Player P, int TileID)
-		{
-			Point16 curTilePos = P.position.ToTileCoordinates16();
-			for (int curX = curTilePos.X; curX < curTilePos.X + 3; curX++)
-			{
-				for (int curY = curTilePos.Y; curY < curTilePos.Y + 4; curY++)
-				{
-					Rectangle tileRect = new Rectangle(curX * 16, curY * 16, 16, 16);
-					if (P.getRect().Intersects(tileRect) && Main.tile[curX, curY].type == TileID)
-					{
-						return true;
-					}
-				}
-			}
-			return false;
+			if (PlayerData.IsPlayeIntersectingTile(PlayerData.LPlayer, TileID.Cactus))
+				player.Hurt(PlayerDeathReason.ByCustomReason(" got spiked to death by a cactus. Ouch!"), Main.rand.Next(6, 8), PlayerData.GetDirection(PlayerData.LPlayer));
 		}
 	}
 }
